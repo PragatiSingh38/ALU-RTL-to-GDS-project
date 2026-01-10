acd9f651c749db334889ce156a8f025c3f486f87
PragatiSingh38 <pragat.singh312@gmail.com>
2026-01-10 21:28:29 +0530

    openSTA_commands.tcl

--- openSTA_commands.tcl
+++ openSTA_commands.tcl
@@ -1,8 +1,11 @@
 read_liberty NangateOpenCellLibrary_typical.lib
-read_verilog opt_my_alu_design.v
+read_verilog detailed_design.v
 link_design mulSelector
 read_sdc alu.sdc
-#report_checks -path_delay max -format full
-#report_checks -path_delay min -format full
+report_checks -path_delay max -format full
+#setup
+report_checks -path_delay min -format full
+#hold
 set_power_activity -global -activity 0.1
 report_power
+#power
